eb5d83799611ffebc4ab8ca504d3d0be5eb37cda
Filip Hanes <filip@hanes.tech>
2023-08-30 10:48:17 +0200

    update blocky

--- workers/blocky.js
+++ workers/blocky.js
@@ -10,9 +10,24 @@ addEventListener('fetch', event => {
       const id = url.pathname.split('/').pop();
       if (id != '') {
         // Handle GET /{id}
-        const body = await BLOCKY.get(id);
+        let body = await BLOCKY.get(id);
+        if (!body) {
+          const res = await fetch('https://ekasa.financnasprava.sk/mdu/api/v1/opd/receipt/find', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ receiptId: id })
+          })
+          body = await res.text()
+          await BLOCKY.put(id, body)
+        }
         return new Response(body, {
-          headers: { 'Content-Type': 'application/json' },
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '86400', // 24 hours
+          },
         });
       }
   
@@ -43,7 +58,7 @@ addEventListener('fetch', event => {
       return new Response(null, {
         headers: {
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST, PUT, OPTIONS',
+          'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
           'Access-Control-Max-Age': '86400', // 24 hours
         },
